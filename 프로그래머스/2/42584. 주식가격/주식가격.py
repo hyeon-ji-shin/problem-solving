f5f2146130076e123753b3e3dc32e5875a89103c
Hyeonji Shin <75267548+hyeon-ji-shin@users.noreply.github.com>
2026-01-03 17:35:20 +0900

    [level 2] Title: 주식가격, Time: 27.52 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,14 +1,21 @@
+# 일일이 가격이 떨어지지 않은 기간을 구하지 않고,
+# '스택'을 통해 가격이 아직 안 떨어진 '인덱스'를 저장하여, 가격이 떨어지면 기간을 구하여 answer 확정시키기
+    
 def solution(prices):
     n = len(prices)
-    answer = []
+    answer = [0] * n # 가격이 떨어지지 않은 기간을 저장할 배열
+    stack = [] # 스택을 이용하여 이전 가격과 현재 가격 비교
     
     for i in range(n):
-        time = 0
-        
-        for j in range(i+1, n):
-            time += 1
-            if prices[j] < prices[i]:
-                break
-        answer.append(time)
+        while stack and prices[stack[-1]] > prices[i]:
+            # i 시점의 가격이 현재 스택의 top 인덱스 가격보다 낮다면: 가격이 떨어졌으므로, 기간 계산
+            prev = stack.pop()
+            answer[prev] = i - prev
+        stack.append(i)
+    
+    # 스택이 남아 있다면: 가격이 떨어지지 않은 경우이므로 최종적으로 기간 계산
+    while stack:
+        prev = stack.pop()
+        answer[prev] = n - 1 - prev
         
     return answer
