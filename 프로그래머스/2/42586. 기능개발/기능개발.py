408a5a5d3b4c9e8062d15453b5c043f5eaa5a62c
Hyeonji Shin <75267548+hyeon-ji-shin@users.noreply.github.com>
2026-01-17 17:37:02 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,23 +1,23 @@
-from collections import deque
+import math
 
 def solution(progresses, speeds):
     
     answer = []
-    q = deque(progresses)
-    sp = deque(speeds)
+    n = len(progresses)
+    # 각 작업의 배포 가능일 계산
+    days_left = [math.ceil((100-progresses[i]) / speeds[i]) for i in range(n)]
     
-    while q:
-        # 하루 경과
-        for i in range(len(q)):
-            q[i] += sp[i]
-        
-        # 배포 가능한 작업 처리
-        cnt = 0
-        while q and q[0] >= 100:
-            q.popleft()
-            sp.popleft()
+    cnt = 0
+    max_day = days_left[0] # 배포가 되지 않은 가장 앞 순서의 기능의 배포 가능일
+    
+    for i in range(n):
+        if days_left[i] <= max_day: # max_day보다 days_left[i](배포 가능일)가 짧은 기능들은 모두 cnt
             cnt += 1
-        if cnt > 0:
+        else: #현재 max_day보다 긴 days_left를 가진 기능을 만난 경우, answer 추가 + cnt, max_day 업데이트
             answer.append(cnt)
+            cnt = 1
+            max_day = days_left[i]
+    
+    answer.append(cnt)
             
     return answer
