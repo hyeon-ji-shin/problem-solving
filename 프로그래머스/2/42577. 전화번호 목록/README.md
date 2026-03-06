3a9082ca56f4d6419e36a0f5fcd06bfa200cbb38
Hyeonji Shin <75267548+hyeon-ji-shin@users.noreply.github.com>
2026-03-06 16:41:06 +0900

    [level 2] Title: 전화번호 목록, Time: 463.31 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=python3#) 
 
 ### 성능 요약
 
-메모리: 29.9 MB, 시간: 96.71 ms
+메모리: 33.8 MB, 시간: 463.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 06일 16:33:48
+2026년 03월 06일 16:41:03
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,10 +1,28 @@
-# phone_book의 원소를 정렬하면, 가까운 숫자끼리 바로 접두어 확인이 수월해진다.
-
 def solution(phone_book):
-    phone_book.sort() # 전화번호부 정렬
+    answer = True
     
-    # 전화번호부에서 연속된 두 번호 비교
-    for i in range(len(phone_book) - 1):
-        if phone_book[i+1].startswith(phone_book[i]): # 접두어 확인을 위해 startswith() 함수를 사용한다.
-            return False # 접두어인 경우가 있다면 false.
-    return True
+    hash_map = {}
+    
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1  # 딕셔너리에서 키 존재 여부 확인 : O(1)
+        
+        # phone_book = ["119", "97674223", "1195524421"]
+        
+        # to
+        
+        # {
+        #  "119":1,
+        #  "97674223":1,
+        #  "1195524421":1
+        # }
+    
+    # 전화번호부에서 번호 하나 꺼내서
+    for phone_number in phone_book:
+        temp = ""
+        # 한글자씩 끊어서 hash_map에 있는 다른 번호(접두어인 경우)에 해당하는 지 확인 (자기 자신 제외)
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                answer = False
+                
+    return answer
