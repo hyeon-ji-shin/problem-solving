1afc6288992f9b4dab6e34307fc8082a93690234
Hyeonji Shin <75267548+hyeon-ji-shin@users.noreply.github.com>
2026-03-07 01:35:31 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,23 +1,28 @@
 import math
+from collections import deque
 
 def solution(progresses, speeds):
+
+    q = deque()
     
+    for progress, speed in zip(progresses, speeds):
+        days = math.ceil((100-progress)/speed)
+        q.append(days)
+        
     answer = []
-    n = len(progresses)
-    # 각 작업의 배포 가능일 계산
-    days_left = [math.ceil((100-progresses[i]) / speeds[i]) for i in range(n)]
     
-    cnt = 0
-    max_day = days_left[0] # 배포가 되지 않은 가장 앞 순서의 기능의 배포 가능일
+    current = q.popleft() # 첫 기능 기준으로 시간
+    count = 1
     
-    for i in range(n):
-        if days_left[i] <= max_day: # max_day보다 days_left[i](배포 가능일)가 짧은 기능들은 모두 cnt
-            cnt += 1
-        else: #현재 max_day보다 긴 days_left를 가진 기능을 만난 경우, answer 추가 + cnt, max_day 업데이트
-            answer.append(cnt)
-            cnt = 1
-            max_day = days_left[i]
-    
-    answer.append(cnt)
+    while q:
+        if q[0] <= current: # 앞 기능보다 빨리 끝난 경우 같이 배포
+            q.popleft()
+            count += 1
+        else: # 새 배포 시작
+            answer.append(count)
+            current = q.popleft()
+            count = 1
             
+    # 마지막 배포까지 반영
+    answer.append(count)
     return answer
