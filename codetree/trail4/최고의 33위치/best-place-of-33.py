bb78e235e1d46455be5b86402f3a44c37052a5a7
Hyeonji Shin <75267548+hyeon-ji-shin@users.noreply.github.com>
2026-05-30 17:13:25 +0900

    [Passed] 최고의 33위치 (47ms, 16MB)

---
+++ codetree/trail4/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,17 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+
+ans = 0
+for i in range (n-2):
+    for j in range (n-2):
+        cnt = 0
+        
+        for x in range(i,i+3):
+            for y in range(j,j+3):
+                cnt += grid[x][y]
+        
+        ans = max(ans, cnt)
+
+print(ans)
