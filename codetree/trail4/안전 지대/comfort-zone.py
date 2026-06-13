460b5201575ab889d58e801d6a56f97ef98e996c
Hyeonji Shin <75267548+hyeon-ji-shin@users.noreply.github.com>
2026-06-13 19:29:11 +0900

    [Passed] 안전 지대 (199ms, 23MB)

--- codetree/trail4/안전 지대/comfort-zone.py
+++ codetree/trail4/안전 지대/comfort-zone.py
@@ -1,33 +1,47 @@
-import sys
-sys.setrecursionlimit(10**6)
-
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
+from collections import deque
+
 ans_k = 1
 ans_area = 0
 
-dx = [1, -1 , 0, 0]
+dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-def dfs(i, j):
+
+def bfs(i, j):
+    q = deque()
+    q.append((i, j))
     visited[i][j] = True
-    for k in range(4):
-        new_x, new_y = i + dx[k], j + dy[k]
-        if 0 <= new_x < n and 0 <= new_y < m and not visited[new_x][new_y] and grid[new_x][new_y] > K:
-            dfs(new_x, new_y)
+
+    while q:
+        x, y = q.popleft()
+
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+
+            if (
+                0 <= nx < n
+                and 0 <= ny < m
+                and not visited[nx][ny]
+                and grid[nx][ny] > K
+            ):
+                visited[nx][ny] = True
+                q.append((nx, ny))
 
 for K in range(1, 101):
-    # visited / 안전 영역 초기화
-    visited = [[False] * m for _ in range (n)]
+    visited = [[False] * m for _ in range(n)]
     area = 0
+
     for i in range(n):
         for j in range(m):
-            if visited[i][j] == False and grid[i][j] > K:
-                dfs(i, j)
+            if not visited[i][j] and grid[i][j] > K:
+                bfs(i, j)
                 area += 1
-    # 만약 더 많은 안전 영역을 갖는 K의 경우가 발견되면 업데이트
+
     if area > ans_area:
         ans_area = area
         ans_k = K
