caf4eb0cc3e6f556238348a9a70fb9fb917d6635
Hyeonji Shin <75267548+hyeon-ji-shin@users.noreply.github.com>
2026-07-22 13:32:23 +0900

    [Passed] 피보나치 수 (46ms, 16MB)

---
+++ codetree/trail4/피보나치 수/fibonacci-number.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+# Please write your code here.
+if N <= 2:
+    print(1)
+else:
+    dp = [0] * (N+1)
+    dp[1] = 1
+    dp[2] = 1
+
+    for i in range(3, N+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+    print(dp[N])
